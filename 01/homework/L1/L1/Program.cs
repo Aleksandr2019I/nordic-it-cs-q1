777400aecafeb7b6e87e66d2cf2575ce37d8e3cf
aleksandr2019i <igr0k@ya.ru>
2019-09-18 20:03:01 +0300

    DZ

---
+++ 01/homework/L1/L1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+
+namespace L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            Console.WriteLine("Enter name");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Hello {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Пока {name}!");
+            Thread.Sleep(1000);
+            Console.WriteLine("Нажмите любую клваишу для завершения работы программы");
+            Console.ReadKey();
+        }
+    }
+}
+
+
