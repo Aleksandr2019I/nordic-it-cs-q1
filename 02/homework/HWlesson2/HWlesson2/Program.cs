ae1d4d9d1ba6b9aa950fc566ac9fd672f0ca6824
aleksandr2019i <igr0k@ya.ru>
2019-09-19 23:57:52 +0300

    залил

---
+++ 02/homework/HWlesson2/HWlesson2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HWlesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
