98de83ddacf6acae778cec980b546a26e2587cdc
aleksandr2019i <igr0k@ya.ru>
2019-09-20 19:56:59 +0300

    last

--- 02/homework/HWlesson2/HWlesson2/Program.cs
+++ 02/homework/HWlesson2/HWlesson2/Program.cs
@@ -4,20 +4,23 @@ namespace HWlesson2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-          
+         static void Main(string[] args)
+         {
 
-            Console.WriteLine("Здравствуйте ,введите первое целое число"); 
-            var N1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("введите второе целое число");
-            var N2 = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Сумма данных чисел = {N1 + N2}");
-            Console.WriteLine($"А разница чисел = {N1 - N2}");
-            Console.WriteLine($"И наконец произведение чисел  = {N1 * N2}");
-            Console.WriteLine("для завершения нажмите любую клавишу");
-            Console.ReadKey();
 
-        }
-    }
+             Console.WriteLine("Здравствуйте ,введите первое целое число"); 
+             var N1 = int.Parse(Console.ReadLine());
+             Console.WriteLine("введите второе целое число");
+             var N2 = int.Parse(Console.ReadLine());
+             Console.WriteLine($"Сумма данных чисел = {N1 + N2}");
+             Console.WriteLine($"А разница чисел = {N1 - N2}");
+             Console.WriteLine($"И наконец произведение чисел  = {N1 * N2}");
+             Console.WriteLine("для завершения нажмите любую клавишу");
+             Console.ReadKey();
+
+         } 
+
+
+       }   
+    
 }
---
+++ 02/homework/HWlesson3/HWlesson3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HWlesson3
+{
+   
+        class Medical
+        {
+            public static string getDozaLekarstva(int ves)
+            {
+                if (ves >= 60) return "Вам нужно принимать по 2 таблетки витамина";
+                else
+                    return "Ваша норма -одна таблетка";
+            }
+        }
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+
+                Console.ReadLine(Medical.getDozaLekarstva(60));
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey(true);
+            }
+        }
+
+}
