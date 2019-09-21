7579fa3f91796e244b353a676a060845263d2276
aleksandr2019i <igr0k@ya.ru>
2019-09-21 16:50:09 +0300

    operand, -for. {}

--- 02/homework/HWlesson3/HWlesson3/Program.cs
+++ 02/homework/HWlesson3/HWlesson3/Program.cs
@@ -8,48 +8,54 @@ namespace HWlesson3
         static void Main(string[] args)
         {
             Console.WriteLine("Введите целое число");
-            var N1 = int.Parse(Console.ReadLine());
+            var firstOperand = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите второе целое число");
-            var N2 = int.Parse(Console.ReadLine());
+            var secondOperand = int.Parse(Console.ReadLine());
             Console.WriteLine("какую операцию вы хотите выполнить: если сложение то введи знак '+',если вычетание то знак '-',умножение знак '*',деление знак '/', остаток по модул знак '%',возвести в степень знак '^'");
-            string select = Console.ReadLine(); // выбор операции
-            if (select == "+")
-                Console.WriteLine($"сумма = {N1 + N2}");
-            else if (select == "-")
-                Console.WriteLine($"вычетание = {N1 - N2}");
-            else if (select == "*")
-                Console.WriteLine($"умножение = {N1 * N2}");
-            else if (select == "/")
-                Console.WriteLine($"деление = {N1 / N2}");
-            else if (select == "%")
-                Console.WriteLine($"по модулю = {N1 % N2}");
-            else if (select == "^")
+            string operation = Console.ReadLine(); // выбор операции
+            if (operation == "+")
             {
-                if (N1 == 0 & N2 == 0) //проверка освнования и степени на 0
+                Console.WriteLine($"сумма = {firstOperand + secondOperand}");
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine($"вычетание = {firstOperand - secondOperand}");
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine($"умножение = {firstOperand * secondOperand}");
+            }
+            else if (operation == "/")
+            {
+                Console.WriteLine($"деление = {firstOperand / secondOperand}");
+            }
+            else if (operation == "%")
+            {
+                Console.WriteLine($"по модулю = {firstOperand % secondOperand}");
+            }
+            else if (operation == "^")
+            {
+                if (firstOperand == 0 & secondOperand == 0) //проверка освнования и степени на 0
                 {
                     Console.WriteLine("Нуль в нулевой степени равен нулю");
                 }
-                else if (N2 == 0) // проверка степени на 0
+                else if (secondOperand == 0) // проверка степени на 0
                 {
                     Console.WriteLine("число в нулевой степени равно Единице");
                 }
-                else if (N1 >= 0 & N2 > 0)
+                else if (firstOperand >= 0 & secondOperand > 0)
                 {
-                    var power = Math.Pow(N1, N2); // через math.pow
-                    Console.WriteLine($"основание '{N1}' в степени '{N2}' = {power}");
-                    // через for
-                    Console.WriteLine("и еще 1 способ возведения в степень");
-                    int c = 1;
-                    for (int i = 0; i < N2; i++)
-                    {
-                        c = c * N1;
-                    }
-                    Console.WriteLine($"{N1}^{N2} = {c}");
+                    var powerOperand = Math.Pow(firstOperand, secondOperand); // через math.pow
+                    Console.WriteLine($"основание '{firstOperand}' в степени '{secondOperand}' = {powerOperand}");       
                 }
             }
             else
+            {
 
                 Console.WriteLine("я незнаю такой операции");
+                
+                
+            }
             Console.WriteLine("для завершения нажмите любую клавишу");
             Console.ReadKey();
         }
