aeee19e414634ae6fa602af058571d98e8bab81b
daria911@yandex.ru <daria911@yandex.ru>
2019-09-26 20:42:03 +0300

    work

---
+++ 04/classwork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Height");
+			var height = double.Parse(Console.ReadLine());
+			Console.WriteLine("Side");
+			var side = double.Parse(Console.ReadLine());
+
+			double sideS = 3 * height * side;
+
+			var sideF = 3 / 2 * height * (height*(Math.Sqrt(3)) + 2 * side);
+
+			var voluem = (Math.Pow(side, 2) / side) * (Math.Sqrt(3));
+
+			Console.WriteLine($"{sideS}, {sideF}, {voluem} ");
+
+
+			Console.ReadKey();
+
+
+
+
+
+
+		}
+	}
+}
