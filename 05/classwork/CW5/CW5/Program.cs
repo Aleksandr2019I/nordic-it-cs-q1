10cc7784e7f8f9c1f3c914d713a0931562d27b28
Direaper <jimac221@gmail.com>
2019-10-01 20:42:20 +0300

    switch

---
+++ 05/classwork/CW5/CW5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CW5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var num = int.Parse(Console.ReadLine());
+			var firstDigit = num / 10;
+			var lastDigit = num % 10;
+			
+			switch (lastDigit)
+			{ 
+				case 1 when firstDigit > 1 || firstDigit == 0:				
+				
+					Console.WriteLine($"{num} год");
+				break;
+				case 2 when firstDigit > 1 || firstDigit == 0:
+				case 3 when firstDigit > 1 || firstDigit == 0:
+				case 4 when firstDigit > 1 || firstDigit == 0:
+					Console.WriteLine($"{num} года");
+				break;
+
+				default:
+					Console.WriteLine($"{num} лет");
+					break;
+
+			}
+			
+			Console.ReadKey();
+		}
+	}
+}
