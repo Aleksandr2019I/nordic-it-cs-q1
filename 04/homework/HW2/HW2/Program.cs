7cc264346c0fd2ad83ca542bda26b5b95861ceda
aleksandr2019i <igr0k@ya.ru>
2019-10-03 16:12:42 +0300

    flags

--- 04/homework/HW2/HW2/Program.cs
+++ 04/homework/HW2/HW2/Program.cs
@@ -20,7 +20,7 @@ namespace HW2
     class Program
     {
         [Flags]
-        enum Container : byte
+        enum Container 
         {
             none = 0x0,
             liter1 = 0x1 <<1,
@@ -33,31 +33,57 @@ namespace HW2
         static void Main()
         {
             var num = ReadDouble();
-            if (num >= 0)
-            {
-                var value = Math.DivRem((int)num, 20, out var balance);
-                Console.WriteLine($"20 л: {value} шт");
-                if (value >= 0)
-                {
-                    var value1 = Math.DivRem(balance, 5, out var balance1);
-                    Console.WriteLine($"5 л: {value1} шт");
 
-                    if (value1 >= 0)
-                    {
-                        var value2 = Math.DivRem(balance1, 1, out var balance2);
-                        Console.WriteLine($"1 л: {value2} шт");
-                    }
-                    
-                }
+            Container flag = Container.none;
 
-               
-                
-                
-            }
+                       var x = num / (int)Container.liter20;
+                       if (x > 0)
+                        {
+                           num %= (int)Container.liter20;
+                           flag |= Container.liter20;
+
+                        }
+            Console.WriteLine($"20 л: {x} шт");
             
-            
-            
-        
+            var y = x / (int)Container.liter5;
+                        if (y > 0)
+                        {
+                            x %= (int)Container.liter5;
+                            flag |= Container.liter5;
+
+                        }
+                       var z = y / (int)Container.liter1;
+                        if (z > 0)
+                        {
+                            y %= (int)Container.liter1;
+                            flag |= Container.liter1;
+
+                        }
+
+                        // в битах которой будет лежать признак наличия контейнера того или иного этого типа(0001 - 1л, 0010 - 5л, 0100 - 20л).
+                        // При выводе,
+
+                        //если бит, отвечающий за наличие хотя бы одного контейнера данного типа, равен 0,
+/*
+                        if (flag | Container.liter1  == 0)
+                        {
+                            Console.WriteLine($"1 Л: {z} шт");
+                        }
+                        if (flag | Container.liter5 == 0)
+                        {
+                            Console.WriteLine($"5 Л: {y} шт");
+                        }
+                        if (flag | Container.liter20 == 0)
+                        {
+                            Console.WriteLine($"20 Л: {x} шт");
+                        }
+                        //строку с данными по этому контейнеру не выводить.
+
+  */          
+            Console.WriteLine($"20 л: {x} шт");
+            Console.WriteLine($"5 Л: {y} шт");        
+           Console.WriteLine($"1 Л: {z} шт");
+
 
 
             Console.ReadKey();
@@ -78,10 +104,12 @@ namespace HW2
             }
             
         }
+ 
+        }
 
 
 
 
-    }
+    
 }
  
