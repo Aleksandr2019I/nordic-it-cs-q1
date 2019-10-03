c2f6d567ca9d5824e6d03b0cb89a1c23addcf02e
aleksandr2019i <igr0k@ya.ru>
2019-10-03 16:57:18 +0300

    ver2

--- 04/homework/HW2/HW2/Program.cs
+++ 04/homework/HW2/HW2/Program.cs
@@ -20,7 +20,7 @@ namespace HW2
     class Program
     {
         [Flags]
-        enum Container 
+        enum ContainerSign
         {
             none = 0x0,
             liter1 = 0x1 <<1,
@@ -29,61 +29,72 @@ namespace HW2
 
         };
 
+        enum ContainerVolume
+        { 
+        
+            volume1 = 1,
+            volume5 = 5,
+            volume20 = 20,
+        };
+
 
         static void Main()
         {
-            var num = ReadDouble();
+            var number = ReadDouble();
 
-            Container flag = Container.none;
+            ContainerSign valueContainer = ContainerSign.none;
 
-                       var x = num / (int)Container.liter20;
-                       if (x > 0)
+                       var countContainer20 = number / (int)ContainerVolume.volume20;
+                       if (countContainer20 > 0)
                         {
-                           num %= (int)Container.liter20;
-                           flag |= Container.liter20;
+                           countContainer20 %= (int)ContainerVolume.volume20;
+                           valueContainer |= ContainerSign.liter20;
 
                         }
-            Console.WriteLine($"20 л: {x} шт");
             
-            var y = x / (int)Container.liter5;
-                        if (y > 0)
+            
+            var countContainer5 = countContainer20 / (int)ContainerVolume.volume5;
+                        if (countContainer5 > 0)
                         {
-                            x %= (int)Container.liter5;
-                            flag |= Container.liter5;
+                            countContainer20 %= (int)ContainerVolume.volume5;
+                            valueContainer |= ContainerSign.liter5;
 
                         }
-                       var z = y / (int)Container.liter1;
-                        if (z > 0)
+                       var countContainer1 = countContainer5 / (int)ContainerVolume.volume1;
+                        if (countContainer1 > 0)
                         {
-                            y %= (int)Container.liter1;
-                            flag |= Container.liter1;
+                            countContainer5 %= (int)ContainerVolume.volume1;
+                            valueContainer |= ContainerSign.liter1;
 
                         }
+            Console.WriteLine($"20 л: {countContainer20} шт");
+            Console.WriteLine($"20 л: {countContainer5} шт");
+            Console.WriteLine($"20 л: {countContainer1} шт");
 
-                        // в битах которой будет лежать признак наличия контейнера того или иного этого типа(0001 - 1л, 0010 - 5л, 0100 - 20л).
-                        // При выводе,
+            // в битах которой будет лежать признак наличия контейнера того или иного этого типа(0001 - 1л, 0010 - 5л, 0100 - 20л).
+            // При выводе,
 
-                        //если бит, отвечающий за наличие хотя бы одного контейнера данного типа, равен 0,
-/*
-                        if (flag | Container.liter1  == 0)
-                        {
-                            Console.WriteLine($"1 Л: {z} шт");
-                        }
-                        if (flag | Container.liter5 == 0)
-                        {
-                            Console.WriteLine($"5 Л: {y} шт");
-                        }
-                        if (flag | Container.liter20 == 0)
-                        {
-                            Console.WriteLine($"20 Л: {x} шт");
-                        }
-                        //строку с данными по этому контейнеру не выводить.
+            //если бит, отвечающий за наличие хотя бы одного контейнера данного типа, равен 0,
+            /*
+                                    if ((valueContainer | Container.liter1)  == 1)
+                                    {
+                                        Console.WriteLine($"1 Л: {z} шт");
+                                    }
+                                    if ((valueContainer | Container.liter5)== 1)
+                                    {
+                                        Console.WriteLine($"5 Л: {y} шт");
+                                    }
+                                    if ((valueContainer | Container.liter20) == 1)
+                                    {
+                                        Console.WriteLine($"20 Л: {x} шт");
+                                    }
+                                    //строку с данными по этому контейнеру не выводить.
 
-  */          
-            Console.WriteLine($"20 л: {x} шт");
-            Console.WriteLine($"5 Л: {y} шт");        
-           Console.WriteLine($"1 Л: {z} шт");
-
+             
+            Console.WriteLine($"20 л: {countContainer20} шт");
+            Console.WriteLine($"5 Л: {countContainer5} шт");        
+           Console.WriteLine($"1 Л: {countContainer1} шт");
+            */
 
 
             Console.ReadKey();
