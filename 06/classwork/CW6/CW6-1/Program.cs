a9fa28c99632fd66ef2213014fd815b4ea541756
Slayer143 <niki2025@mail.ru>
2019-10-03 21:45:34 +0300

    ver4

---
+++ 06/classwork/CW6/CW6-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CW6_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var marks = new[]
+			{
+				new [] { 2,3,3,2,3},
+				new [] {2,4,5,3},
+				null,
+				new [] {5,5,5,5},
+				new [] {4},
+			};
+
+			double sumNed = 0;
+			for (var i = 0; i < marks.Length; i++)
+
+			{
+
+				if (marks[i] == null)
+				{
+					Console.WriteLine($"The average mark for day #{i}, is N/A");
+					continue;
+				}
+				double bal = 0; 
+
+				for (var j = 0; j < marks[i].Length; j++)
+					
+				{
+					
+						 bal += marks[i][j] ;	
+						
+					
+				}
+
+				double sum = bal / marks[i].Length;
+				Console.WriteLine($"The average mark for day #{i}, is {sum}");
+
+				 sumNed = sum / marks[i].Length;
+				
+			}
+			Console.WriteLine($"The average mark for all the week is {sumNed} ");
+
+			Console.ReadKey();
+		}
+	}
+}
