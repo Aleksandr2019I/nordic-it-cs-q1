6fc51a396c281d47d1319db752e62763374388af
Slayer143 <niki2025@mail.ru>
2019-10-03 18:59:06 +0300

    VC1

--- 04/homework/HW2/HW2/Program.cs
+++ 04/homework/HW2/HW2/Program.cs
@@ -3,7 +3,7 @@
 namespace HW2
 {
 
-    /*
+	/*
      Написать консольное приложение, которое будет спрашивать, “Какой объем
 сока (в литрах) требуется упаковать?”.
 Затем оно будет рассчитывать и выдавать в качестве ответа необходимое
@@ -17,108 +17,116 @@ namespace HW2
 При выводе, если бит, отвечающий за наличие хотя бы одного контейнера
 данного типа, равен 0, строку с данными по этому контейнеру не выводить.
          */
-    class Program
-    {
-        [Flags]
-        enum ContainerSign
-        {
-            none = 0x0,
-            liter1 = 0x1 <<1,
-            liter5 = 0x1 <<2,
-            liter20 = 0x1 <<3,
+	class Program
+	{
+		[Flags]
+		enum ContainerSign
+		{
+			none = 0x0,
+			liter1 = 0x1 << 1,
+			liter5 = 0x1 << 2,
+			liter20 = 0x1 << 3,
 
-        };
+		};
 
-        enum ContainerVolume
-        { 
-        
-            volume1 = 1,
-            volume5 = 5,
-            volume20 = 20,
-        };
+		enum ContainerVolume
+		{
 
-
-        static void Main()
-        {
-            var numberOfLiters = ReadDouble();
-
-            ContainerSign valueContainer = ContainerSign.none;
-
-                       var countContainer20 = numberOfLiters / (int)ContainerVolume.volume20;
-                       
-                       if (countContainer20 > 0)
-                        {
-                            numberOfLiters %= (int)ContainerVolume.volume20; //numberOfLiters ненужное присваивание значения
-                            valueContainer |= ContainerSign.liter20;
-
-                        }
-            
-   
-            var countContainer5 = countContainer20 / (int)ContainerVolume.volume5;
-                        if (countContainer5 > 0)
-                        {
-                countContainer20 %= (int)ContainerVolume.volume5;
-                            valueContainer |= ContainerSign.liter5;
-
-                        }
-                       var countContainer1 = countContainer5 / (int)ContainerVolume.volume1;
-                        if (countContainer1 > 0)
-                        {
-                         countContainer5 %= (int)ContainerVolume.volume1;
-                            valueContainer |= ContainerSign.liter1; //valueContainer ненужное присваивание значения
-
-            }
-
-            Console.WriteLine($"20 л: {countContainer20}шт");
-            Console.WriteLine($"5 л: {countContainer5} шт");
-          Console.WriteLine($"1 л: {countContainer1} шт");
-
-            // в битах которой будет лежать признак наличия контейнера того или иного этого типа(0001 - 1л, 0010 - 5л, 0100 - 20л).
-            // При выводе,
-
-            //если бит, отвечающий за наличие хотя бы одного контейнера данного типа, равен 0,
-         
-                                    if ((valueContainer | ContainerSign.liter1) != 0)
-                                    {
-                                        Console.WriteLine($"1 Л: { ContainerSign.liter1} шт");
-                                    }
-                                    if ((valueContainer | ContainerSign.liter5) != 0)
-                                    {
-                                        Console.WriteLine($"5 Л: { ContainerSign.liter5} шт");
-                                    }
-                                    if ((valueContainer | ContainerSign.liter20) != 0)
-                                    {
-                                        Console.WriteLine($"20 Л: { ContainerSign.liter20} шт");
-                                    }
-                                    //строку с данными по этому контейнеру не выводить.
+			volume1 = 1,
+			volume5 = 5,
+			volume20 = 20,
+		};
 
 
-          
+		static void Main()
+		{
+			
+			var numberOfLiters = ReadDouble();
 
-            Console.ReadKey();
-        }
-        static double ReadDouble()
-        {
-            while (true)
-            {
-                Console.Write("Какой объем сока (в литрах) требуется упаковать? ");
-                if (double.TryParse(Console.ReadLine(), out var input)) //проверка на число
-                {
-                    Console.WriteLine($"Ваше число {input}");
-                    Console.WriteLine($"Вам потребуются следующие контейнеры:");
-                    return input;
-                    
-                }
-                Console.WriteLine("Wrong value");
-            }
-            
-        }
- 
-        }
+			ContainerSign valueContainer = ContainerSign.none;
+
+			var countContainer20 = Math.Floor (numberOfLiters / (int)ContainerVolume.volume20);
+
+			if (countContainer20 > 0)
+			{
+				numberOfLiters %= (int)ContainerVolume.volume20; //numberOfLiters ненужное присваивание значения
+				valueContainer |= ContainerSign.liter20;
+
+			}
+
+
+			var countContainer5 = Math.Floor (numberOfLiters / (int)ContainerVolume.volume5);
+			if (countContainer5 > 0)
+			{
+				numberOfLiters %= (int)ContainerVolume.volume5;
+				valueContainer |= ContainerSign.liter5;
+
+			}
+			var countContainer1 = numberOfLiters / (int)ContainerVolume.volume1;
+			if (countContainer1 > 0)
+			{
+				numberOfLiters %= (int)ContainerVolume.volume1;
+				valueContainer |= ContainerSign.liter1; //valueContainer ненужное присваивание значения
+
+			}
+
+	
+
+			// в битах которой будет лежать признак наличия контейнера того или иного этого типа(0001 - 1л, 0010 - 5л, 0100 - 20л).
+			// При выводе,
+
+			//если бит, отвечающий за наличие хотя бы одного контейнера данного типа, равен 0,
+
+			// 0000
+			// 0001
+			// 0010
+			// 0011
+			// 0100
+			// 0101
+			// 0110
+			// 0111
+
+			// 0001
+			if ((valueContainer & ContainerSign.liter1) == ContainerSign.liter1) //
+			{
+				Console.WriteLine($"1 л: {countContainer1} шт");
+			}
+			if ((valueContainer & ContainerSign.liter5) == ContainerSign.liter5)
+			{
+				Console.WriteLine($"5 л: {countContainer5} шт");
+			}
+			if ((valueContainer & ContainerSign.liter20) == ContainerSign.liter20)
+			{
+				Console.WriteLine($"20 л: {countContainer20}шт");
+			}
+			//строку с данными по этому контейнеру не выводить.
 
 
 
 
-    
+			Console.ReadKey();
+		}
+		static double ReadDouble()
+		{
+			while (true)
+			{
+				Console.Write("Какой объем сока (в литрах) требуется упаковать? ");
+				if (double.TryParse(Console.ReadLine(), out var input)) //проверка на число
+				{
+					Console.WriteLine($"Ваше число {input}");
+					Console.WriteLine($"Вам потребуются следующие контейнеры:");
+					return input;
+
+				}
+				Console.WriteLine("Wrong value");
+			}
+
+		}
+
+	}
+
+
+
+
+
 }
- 
