b2a6544f0bb4f119cdf46df9f008ab8bf4afcddb
aleksandr2019i <igr0k@ya.ru>
2019-10-08 01:16:47 +0300

    even

--- 06/homework/HW6/HW6/Program.cs
+++ 06/homework/HW6/HW6/Program.cs
@@ -6,52 +6,52 @@ namespace HW6
     {
         static void Main()
         {
+            int evenNumber = Even();
+            Console.WriteLine($"числе  содержится следующее количество четных цифр:{evenNumber}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+        static int Even()
+        {
+            var calculationResult = ReadNumbers();
+            var count = 0;
+            while (calculationResult > 0)
+            {
+                calculationResult = Math.DivRem(calculationResult, 10, out int rezult);
+                if ((rezult % 2) == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static int ReadNumbers()
+        {
             Console.WriteLine("Введите положительное натуральное число не более 2 миллиардов:");
             while (true)
             {
                 try
                 {
-                    int number = int.Parse(Console.ReadLine());
-
-                    if (number > 0)
-                    {
-                        var count = 0;
-                        var numbers = 0;                           
-                            numbers = Math.DivRem(number, 10, out int rezult);
-
-                            if ((rezult % 2) == 0)
-                            {
-                                 count++;
-                            
-                            }
-                            
-                        Console.WriteLine($"в числе {number} содержится следующее количество четных цифр:{count} ");
-                        Console.WriteLine("Нажмите любую клавишу для выхода...");
-                        Console.ReadKey();
-                        break;
-                    }
-                    else
+                    int naturalNumber = int.Parse(Console.ReadLine());
+                    if (naturalNumber < 0)
                     {
                         Console.WriteLine("Введено неверное значение! Попробуйте ещё раз:");
                     }
+                    else
+                    {
+                        return naturalNumber;
+                    }
                 }
-                catch (Exception ex)
+                catch (OverflowException)
                 {
-                    Console.WriteLine($"Ошибка {ex.Message}! Попробуйте еще раз:");
-
-                  
+                    Console.WriteLine("Ошибка System.OverflowException! Попробуйте ещё раз:");
                 }
-                catch (Exception ex)
+                catch (FormatException)
                 {
-                    Console.WriteLine($"Ошибка {ex.Message}! Попробуйте еще раз:");
+                    Console.WriteLine("Ошибка System.FormatException! Попробуйте ещё раз:");
                 }
             }
-            
         }
-
-
     }
-
-
 }
 
--- 06/homework/HW6/test/Program.cs
+++ 06/homework/HW6/test/Program.cs
@@ -6,26 +6,52 @@ namespace test
     {
         static void Main()
         {
-            var x = TopL();
-            Console.WriteLine($"{x}");
+            int evenNumber = Even();
+            Console.WriteLine($"числе  содержится следующее количество четных цифр:{evenNumber}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
             Console.ReadKey();
         }
-        static int TopL()
+        static int Even()
         {
-            var number = int.Parse(Console.ReadLine());
+            var calculationResult = ReadNumbers();
             var count = 0;
-
-            while (number > 0)
-            { 
-            number = Math.DivRem(number, 10, out int rez);
-            if ((rez % 2) == 0)
+            while (calculationResult > 0)
             {
-                count++;
-            }
-            
+                calculationResult = Math.DivRem(calculationResult, 10, out int rezult);
+                if ((rezult % 2) == 0)
+                {
+                    count++;
+                }
             }
             return count;
         }
-           
+        static int ReadNumbers()
+        {
+            Console.WriteLine("Введите положительное натуральное число не более 2 миллиардов:");
+            while (true)
+            {
+                try
+                {
+                    int naturalNumber = int.Parse(Console.ReadLine());
+                    if (naturalNumber < 0)
+                    {
+                        Console.WriteLine("Введено неверное значение! Попробуйте ещё раз:");
+                    }
+                    else
+                    {
+                        return naturalNumber;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Ошибка System.OverflowException! Попробуйте ещё раз:");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка System.FormatException! Попробуйте ещё раз:");
+                }
+            }
+        }
+
     }
 }
