7809978d59ca7fc6e94762f75bf25374a50e00cd
aleksandr2019i <igr0k@ya.ru>
2019-10-07 18:53:59 +0300

    ok

--- 06/classwork/CW6/CW6-1/Program.cs
+++ 06/classwork/CW6/CW6-1/Program.cs
@@ -2,49 +2,46 @@
 
 namespace CW6_1
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var marks = new[]
-			{
-				new [] { 2,3,3,2,3},
-				new [] {2,4,5,3},
-				null,
-				new [] {5,5,5,5},
-				new [] {4},
-			};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var marks = new[]
+            {
+                new [] { 2,3,3,2,3},
+                new [] {2,4,5,3},
+                null,
+                new [] {5,5,5,5},
+                new [] {4},
+            };
 
-			double sumNed = 0;
-			for (var i = 0; i < marks.Length; i++)
+            double sumNed = 0.0;
+            for (var i = 0; i < marks.Length; i++)
 
-			{
+            {
 
-				if (marks[i] == null)
-				{
-					Console.WriteLine($"The average mark for day #{i}, is N/A");
-					continue;
-				}
-				double bal = 0; 
+                if (marks[i] == null)
+                {
+                    Console.WriteLine($"The average mark for day #{i}, is N/A");
+                    continue;
+                }
+                double bal = 0;
 
-				for (var j = 0; j < marks[i].Length; j++)
-					
-				{
-					
-						 bal += marks[i][j] ;	
-						
-					
-				}
+                for (var j = 0; j < marks[i].Length; j++)
 
-				double sum = bal / marks[i].Length;
-				Console.WriteLine($"The average mark for day #{i}, is {sum}");
+                {
+                    bal += marks[i][j];
+                }
 
-				 sumNed = sum / marks[i].Length;
-				
-			}
-			Console.WriteLine($"The average mark for all the week is {sumNed} ");
+                double sum = bal / marks[i].Length;
+                Console.WriteLine("The average mark for day #{0}, is {1:0.0}",i,sum);
 
-			Console.ReadKey();
-		}
-	}
+                sumNed = sum / marks[i].Length;
+
+            }
+            Console.WriteLine("The average mark for all the week is {0:0.0}",sumNed);
+
+            Console.ReadKey();
+        }
+    }
 }
