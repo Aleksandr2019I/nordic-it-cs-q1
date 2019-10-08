40fde9e8ce646940f6e11b4f9c23c7e16da15924
michaell1ight <michail.golovkov@gmail.com>
2019-10-08 21:58:33 +0300

    СР

---
+++ 02/classwork/lesson2/lesson2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{ // comment
+		  /*
+		   comment 2 cntrl+k+c ; cntrl+k+u выделяем строку и 
+		   */
+			Console.Writeline("hello world!");
+			Console.ReadKey();
+
+
+
+		}
+	}
+}
