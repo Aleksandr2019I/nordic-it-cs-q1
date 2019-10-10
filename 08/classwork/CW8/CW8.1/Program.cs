6347cd569fa664af4860c992ac19180931ffc4cb
michaell1ight <michail.golovkov@gmail.com>
2019-10-10 21:55:21 +0300

    задачки

---
+++ 08/classwork/CW8/CW8.1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CW8._1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var capitals = new Dictionary<string, string>
+			{
+				["Russia"] = "Moscow",
+				["USA"] = "Washington",
+				["Great britain"] = "London"
+			};
+			
+
+			while (true)
+			{
+				KeyValuePair<string, string> randomElement = GetRandomElement(capitals);
+				Console.WriteLine($"Какая столица страны  {randomElement.Key} ");
+				 var val = Console.ReadLine();
+				if (val == randomElement.Value)
+				{
+					Console.WriteLine("Вы правы, молодец");
+				}
+				else
+				{
+					Console.WriteLine("Вы ошиблись, пока.");
+					break;
+				}
+
+			}
+
+			Console.ReadKey();
+		}
+
+		static KeyValuePair<string, string> GetRandomElement(Dictionary<string, string> capitals)
+		{
+			var random = new Random();
+			var i = 0;
+			var index = random.Next(0, capitals.Count);
+
+			foreach (var pair in capitals)
+			{
+				if (i == index)
+				{
+					return pair;
+				}
+				i++;
+			}
+			throw new IndexOutOfRangeException();
+		}
+
+	}
+}
---
+++ 08/classwork/CW8/очередь/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace очередь
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var queue = new Queue<int>();
+			while (true)
+			{
+				var num = Console.ReadLine();
+				if (num == "run")
+				{
+					while (queue.Count > 0)
+					{
+						var x = Math.Sqrt(queue.Dequeue());
+						Console.WriteLine($"{x}");
+					}
+				}
+				else if (num == "exit")
+				{
+					var y = queue.Count;
+					Console.WriteLine($"{y}");
+					break;
+				}
+				else
+				{
+					queue.Enqueue(int.Parse(num));
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
