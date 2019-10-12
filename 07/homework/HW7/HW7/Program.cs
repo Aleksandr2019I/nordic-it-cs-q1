b19f4cbaa917272aa69917907e7b769de2a1b97c
aleksandr2019i <igr0k@ya.ru>
2019-10-12 18:30:07 +0300

    ДЗ1

---
+++ 07/homework/HW7/HW7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace HW7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку из нескольких слов:");
+            
+            while (true)
+            { 
+            string word = Console.ReadLine();
+
+            string[] massiv = word.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                
+
+            if (massiv.Length < 2)
+            {
+                Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
+            }
+            else
+            {
+                    for (int i = 0; i < massiv.Length; i++)
+                    {
+
+                    }
+                    Console.WriteLine($"Количество слов, начинающихся с буквы 'А': ");
+                    
+            }
+
+                Console.WriteLine("Нажмите любую клавишу для выхода...");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
