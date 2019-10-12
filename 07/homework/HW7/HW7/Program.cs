81f14addee19c53bb642f55f7c166363c564b3c1
aleksandr2019i <igr0k@ya.ru>
2019-10-12 22:55:44 +0300

    k,d

--- 07/homework/HW7/HW7/Program.cs
+++ 07/homework/HW7/HW7/Program.cs
@@ -7,33 +7,50 @@ namespace HW7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите строку из нескольких слов:");
-            
             while (true)
-            { 
-            string word = Console.ReadLine();
-
-            string[] massiv = word.Split(' ',StringSplitOptions.RemoveEmptyEntries);
-                
-
-            if (massiv.Length < 2)
             {
-                Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
-            }
-            else
-            {
+                string word = Readtext();
+                string[] massiv = word.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (massiv.Length < 2)
+                {
+                    Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
+                }
+                else
+                {
+                    int j = default;
                     for (int i = 0; i < massiv.Length; i++)
                     {
-
+                        if (massiv[i].StartsWith('A') | massiv[i].StartsWith('a') | massiv[i].StartsWith('А') | massiv[i].StartsWith('а'))
+                        {
+                            j++;
+                        }
                     }
-                    Console.WriteLine($"Количество слов, начинающихся с буквы 'А': ");
-                    
-            }
-
-                Console.WriteLine("Нажмите любую клавишу для выхода...");
+                    Console.WriteLine($"Количество слов, начинающихся с буквы 'А':{j}");
+                    Console.WriteLine("Нажмите любую клавишу для выхода...");
+                }
                 Console.ReadKey();
             }
-            
+        }
+        static string Readtext()
+        {
+            Console.WriteLine("Введите строку из нескольких слов:");
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    return input;
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
+                catch (OutOfMemoryException ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
+            }
         }
     }
 }
