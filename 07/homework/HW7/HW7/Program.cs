e153d11a4ee2c0c381bd7d1197b6990aa9490986
Direaper <jimac221@gmail.com>
2019-10-15 18:48:21 +0300

    last

--- 07/homework/HW7/HW7/Program.cs
+++ 07/homework/HW7/HW7/Program.cs
@@ -3,54 +3,41 @@ using System.Text;
 
 namespace HW7
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Введите строку из нескольких слов:");
-            while (true)
-            {
-                string word = Readtext();
-                string[] massiv = word.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            
-                if (massiv.Length < 2)
-                {
-                    Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
-                }
-                else
-                {
-                    int j = default;
-                    for (int i = 0; i < massiv.Length; i++)
-                    {
-                        if (massiv[i].StartsWith('A') | massiv[i].StartsWith('a') | massiv[i].StartsWith('А') | massiv[i].StartsWith('а'))
-                        {
-                            j++;
-                        }
-                    }
-                    Console.WriteLine($"Количество слов, начинающихся с буквы 'А':{j}");
-                    Console.WriteLine("Нажмите любую клавишу для выхода...");
-                    Console.ReadKey();
-                }
-            }
-        }
-        static string Readtext()
-        {
-            while (true)
-            {
-                try
-                {
-                    string input = Console.ReadLine();
-                    return input;
-                }
-                catch (ArgumentOutOfRangeException ex)
-                {
-                    Console.WriteLine($"{ex.Message}");
-                }
-                catch (OutOfMemoryException ex)
-                {
-                    Console.WriteLine($"{ex.Message}");
-                }
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите строку из нескольких слов:");
+
+			var word = Readtext();
+
+			int j = default(int);
+			for (int i = 0; i < word.Length; i++)
+			{
+				if (word[i].ToLowerInvariant().StartsWith('a') | word[i].ToLowerInvariant().StartsWith('а'))
+				{
+					j++;
+				}
+			}
+			Console.WriteLine($"Количество слов, начинающихся с буквы 'А':{j}");
+			Console.WriteLine("Нажмите любую клавишу для выхода...");
+			Console.ReadKey();
+		}
+		static string[] Readtext()
+		{
+			while (true)
+			{
+				
+					string input = Console.ReadLine();
+					string[] massiv = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+					if (massiv.Length < 2 |)
+					{
+						Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
+					}
+					return massiv;
+				
+		
+			}
+		}
+	}
 }
