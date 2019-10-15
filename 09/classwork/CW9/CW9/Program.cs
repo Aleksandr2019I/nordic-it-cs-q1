9dcc97e84b7b396d4fcc0bdb4b182c0fdc03e8ad
Direaper <jimac221@gmail.com>
2019-10-15 22:03:59 +0300

    9

--- 09/classwork/CW9/CW9/Program.cs
+++ 09/classwork/CW9/CW9/Program.cs
@@ -1,14 +1,39 @@
 ﻿using System;
-
+using System.Diagnostics;
 namespace CW9
 {
-	
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			int x = 0;
-			x = x + 1;
+			Random random = new Random();
+			int[] array = new int[99];
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = random.Next();
+			}
+			mass(array);
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine($"{array[i]}");
+			}
+		}
+		static void mass(int[] array)
+		{
+			for (int i = 0; i < array.Length - 1; i++)
+			{
+				var limit = array.Length - 1 - i;
+				for (int j = 0; j < limit; j++)
+				{
+					if (array[j] < array[j + 1])
+					{
+						var temp = array[j + 1]; //1
+						array[j + 1] = array[j]; // 0
+						array[j] = temp; // 1
+					};
+				}
+			}
 		}
 	}
 }
