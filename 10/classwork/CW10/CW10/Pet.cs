d6b1febd1055ed5c8b8cde63ccea6da1237dc79f
MakarevichAV <redishtale@gmail.com>
2019-10-17 22:11:01 +0300

    asd

---
+++ 10/classwork/CW10/CW10/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CW10
+{
+	class Pet
+	{
+		// У публичных полей название с БОЛЬШОЙ буквы.
+		// 
+		public string Kind;
+
+		public string Name;
+
+		public char Sex;
+
+		public int Age;
+
+		public Pet()
+		{
+		}
+		public string Description
+		{
+			get
+			{
+				return $"The lalal {Name},{Kind},{Sex},{Age}";
+			}
+		}
+
+		public string Description =>
+			$"The lalal {Name},{Kind},{Sex},{Age}";
+
+	}
+}
---
+++ 10/classwork/CW10/CW10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CW10
+{
+	class Program
+	{
+
+
+		static void Main(string[] args)
+		{
+			Pet pet = new Pet()
+			{
+				Name = "The Dog",
+				Kind = "Dog",
+				Sex = 'M',
+				Age = 10
+			};
+
+			Console.WriteLine($"The lalal {pet.Name},{pet.Kind},{pet.Sex},{pet.Age}");
+			
+
+			Console.WriteLine(pet.Description);
+
+			Pet pet1 = new Pet();
+			pet1.Name = "The Cat";
+			pet1.Kind = "Cat";
+			pet1.Sex = 'W';
+			pet1.Age = 22;
+
+		}
+	}
+}
