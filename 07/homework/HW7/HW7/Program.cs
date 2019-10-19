ef549d3f0cf163f68f847d4f8c40362b235221ec
aleksandr2019i <igr0k@ya.ru>
2019-10-19 14:26:41 +0300

    last

--- 07/homework/HW7/HW7/Program.cs
+++ 07/homework/HW7/HW7/Program.cs
@@ -3,41 +3,41 @@ using System.Text;
 
 namespace HW7
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Введите строку из нескольких слов:");
-			var word = Readtext();
-			int j = default;
-			for (int i = 0; i < word.Length; i++)
-			{
-				if (word[i].ToLowerInvariant().StartsWith('a') | word[i].ToLowerInvariant().StartsWith('а'))
-				{
-					j++;
-				}
-			}
-			Console.WriteLine($"Количество слов, начинающихся с буквы 'А':{j}");
-			Console.WriteLine("Нажмите любую клавишу для выхода...");
-			Console.ReadKey();
-		}
-		static string[] Readtext()
-		{
-			while (true)
-			{
-				string input = Console.ReadLine();
-				if (String.IsNullOrWhiteSpace(input))
-				{
-					Console.WriteLine("Строка пустая");
-					continue;
-				};
-				string[] massiv = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-				if (massiv.Length < 2)
-				{
-					Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
-				}
-				return massiv;
-			}
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку из нескольких слов:");
+            var word = Readtext();
+            int j = default;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i].ToLowerInvariant().StartsWith('a') | word[i].ToLowerInvariant().StartsWith('а'))
+                {
+                    j++;
+                }
+            }
+            Console.WriteLine($"Количество слов, начинающихся с буквы 'А':{j}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+        static string[] Readtext()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Строка пустая");
+                    continue;
+                };
+                string[] massiv = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (massiv.Length < 2)
+                {
+                    Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз:");
+                }
+                return massiv;
+            }
+        }
+    }
 }
--- 08/homework/HW8/HW8/Program.cs
+++ 08/homework/HW8/HW8/Program.cs
@@ -6,7 +6,7 @@ namespace HW8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
         }
     }
 }
