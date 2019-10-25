fcc11dc4dfdc5eab7dc524dae86099d400d8f2c3
aleksandr2019i <igr0k@ya.ru>
2019-10-25 16:23:34 +0300

    25

--- 08/homework/HW8/HW8/Program.cs
+++ 08/homework/HW8/HW8/Program.cs
@@ -2,50 +2,46 @@
 using System.Collections.Generic;
 namespace HW8
 {
-	class Program
-	{
-		static void Main()
-		{
-			var text = Console.ReadLine();
-			char[] array = text.ToCharArray();
-			Stack<char> stack = new Stack<char>();
-			for (int i = 0; i < array.Length; i++)
-			{
-				switch (array[i])
-				{
-					case '(':
-					case '[':
-						stack.Push(array[i]);
-						break;
-					case ')':
-						if (stack.Count == 0)
-						{
-							break;
-						}
-						else if (stack.Peek() == '(')
-						{
-							stack.Pop();
-						}
-						else
-						{
-							// что-то пошло не так
-						}
-						break;
-					case ']':
-						if (stack.Peek() == '[')
-						{
-							stack.Pop();
-						}
-						else
-						{
-							// что-то пошло не так
-						}
-						break;
-					default:
-						break;
-				}
-			}
-			Console.ReadKey();
-		}
-	}
+    class Program
+    {
+        static void Main()
+        {
+            var text = "[]()"; //Console.ReadLine();
+            char[] array = text.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                switch (array[i])
+                {
+                    case '(':
+                    case '[':
+                        stack.Push(array[i]);
+                        break;
+                    case ')':
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        else if (stack.Peek() == '(')
+                        {
+                            stack.Pop();
+                        }
+                        break;
+                    case ']':
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        else if (stack.Peek() == '[')
+                        {
+                            stack.Pop();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
 }
---
+++ 11/classwork/CW10/CW10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CW11
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		//	Pet pet2 = new Pet();
+		//	pet2.Name = "The Cat";
+		//	pet2.Kind = "Cat";
+		//	pet2.Sex = 'W';
+		//	pet2.DateOfBirth = new DateTime(2015, 3, 23);
+		//	pet2.Display(true);
+		//	Console.WriteLine(pet2.Description);
+
+		//	Pet pet1 = new Pet()
+		//	{
+		//		Name = "The Dog",
+		//		Kind = "Dog",
+		//		Sex = 'M',
+		//		DateOfBirth = new DateTime(2018, 9, 15)
+		//};
+			////Console.WriteLine($"The lalal {pet.Name},{pet.Kind},{pet.Sex},{pet.Age}");
+			//pet1.Display(false);
+
+			var nameType = new Pet(name: Console.ReadLine(), kind: Console.ReadLine());
+			Console.WriteLine($"{nameType.ShortDescription} ");
+
+			Console.ReadKey();
+		}
+	}
+}
