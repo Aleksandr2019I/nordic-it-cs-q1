f3fcf421a48c0425775468be6a75fbf993cb7963
aleksandr2019i <igr0k@ya.ru>
2019-10-25 17:00:00 +0300

    last

--- 08/homework/HW8/HW8/Program.cs
+++ 08/homework/HW8/HW8/Program.cs
@@ -6,40 +6,48 @@ namespace HW8
     {
         static void Main()
         {
-            var text = "[]()"; //Console.ReadLine();
+            var text = Console.ReadLine();
             char[] array = text.ToCharArray();
             Stack<char> stack = new Stack<char>();
             for (int i = 0; i < array.Length; i++)
             {
-                switch (array[i])
+                try
                 {
-                    case '(':
-                    case '[':
-                        stack.Push(array[i]);
-                        break;
-                    case ')':
-                        if (stack.Count == 0)
-                        {
+                    switch (array[i])
+                    {
+                        case '(':
+                        case '[':
+                            stack.Push(array[i]);
                             break;
-                        }
-                        else if (stack.Peek() == '(')
-                        {
-                            stack.Pop();
-                        }
-                        break;
-                    case ']':
-                        if (stack.Count == 0)
-                        {
+                        case ')':
+                            if (stack.Count == 0)
+                            {
+                                throw new InvalidOperationException();
+                            }
+                            if (stack.Peek() == '(')
+                            {
+                                stack.Pop();
+                            }
                             break;
-                        }
-                        else if (stack.Peek() == '[')
-                        {
-                            stack.Pop();
-                        }
-                        break;
-                    default:
-                        break;
+                        case ']':
+                            if (stack.Count == 0)
+                            {
+                               throw new InvalidOperationException();
+                            }
+                            if (stack.Peek() == '[')
+                            {
+                                stack.Pop();
+                            }
+                            break;
+                        default:
+                            break;
+                    }
                 }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("стек пустой");
+                }
+
             }
             Console.ReadKey();
         }
