6dcb45e34d7db4700023a41f4e8b24bf060e52a9
Smithprojects <anton29141@gmail.com>
2019-10-29 22:06:02 +0300

    frt

--- 08/homework/HW8/HW8/Program.cs
+++ 08/homework/HW8/HW8/Program.cs
@@ -2,80 +2,78 @@
 using System.Collections.Generic;
 namespace HW8
 {
-    class Program
-    {
-        static void Main()
-        {
-            var text = ReadText();
+	class Program
+	{
+		static void Main()
+		{
+			var text = ReadText();
 			bool result = check(text);
 			Console.WriteLine($"{ result}");
-            Console.ReadKey();
-        }
+			Console.ReadKey();
+		}
 
-        static string ReadText()
-        {
-            while (true)
-            {
-                Console.WriteLine("Введите строку:");
-                var text = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(text))
-                {
-                    Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
-                    continue;
-                }
-                return text;
-            }
-        }
+		static string ReadText()
+		{
+			while (true)
+			{
+				Console.WriteLine("Введите строку:");
+				var text = Console.ReadLine();
+				if (string.IsNullOrWhiteSpace(text))
+				{
+					Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
+					continue;
+				}
+				return text;
+			}
+		}
 		static bool check(string text)
 		{
 			Stack<char> stack = new Stack<char>();
 
+
+			for (int i = 0; i < text.Length; i++)
+			{
+
+				switch (text[i])
+				{
+					case '(':
+					case '[':
+						stack.Push(text[i]);
+						break;
+					case ')':
+					//if (stack.Count == 0)
+					//{
+					//    throw new InvalidOperationException();
+					//}
+					//if (stack.Peek() == '(')
+					//{
+					//    stack.Pop();
+					//}
+					//break;
+					case ']':
+						if (stack.Count == 0)
+						{
+							return false;
+						}
+						if (stack.Peek() == '[' || stack.Peek() == '[')
+						{
+							stack.Pop();
+						}
+						else
+						{
+
+							return false;
+						}
+
+						break;
+					default:
+						break;
+				}
+				return true;
+
 			
-            for (int i = 0; i<text.Length; i++)
-            {
-                try
-                {
-                    switch (text[i])
-                    {
-                        case '(':
-                        case '[':
-                            stack.Push(text[i]);
-                            break;
-                        case ')':
-                        //if (stack.Count == 0)
-                        //{
-                        //    throw new InvalidOperationException();
-                        //}
-                        //if (stack.Peek() == '(')
-                        //{
-                        //    stack.Pop();
-                        //}
-                        //break;
-                        case ']':
-                            if (stack.Count == 0)
-                            {
-                                throw new InvalidOperationException();
-	}
-							if (stack.Peek() == '[' || stack.Peek() == '[')
-							{
-								stack.Pop();
-							}
-							else
-							{
-								throw new InvalidOperationException();
-							}
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                catch (InvalidOperationException)
-                {
-                    Console.WriteLine("стек пустой");
-                }
-            }
-			return text;
+
 
 		}
-    }
+	}
 }
