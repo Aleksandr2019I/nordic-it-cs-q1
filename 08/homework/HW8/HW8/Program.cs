64075619f0d6afa551443c364607e70412e55788
aleksandr2019i <igr0k@ya.ru>
2019-10-30 23:21:00 +0300

    1

--- 08/homework/HW8/HW8/Program.cs
+++ 08/homework/HW8/HW8/Program.cs
@@ -2,78 +2,61 @@
 using System.Collections.Generic;
 namespace HW8
 {
-	class Program
-	{
-		static void Main()
-		{
-			var text = ReadText();
-			bool result = check(text);
-			Console.WriteLine($"{ result}");
-			Console.ReadKey();
-		}
-
-		static string ReadText()
-		{
-			while (true)
-			{
-				Console.WriteLine("Введите строку:");
-				var text = Console.ReadLine();
-				if (string.IsNullOrWhiteSpace(text))
-				{
-					Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
-					continue;
-				}
-				return text;
-			}
-		}
-		static bool check(string text)
-		{
-			Stack<char> stack = new Stack<char>();
-
-
-			for (int i = 0; i < text.Length; i++)
-			{
-
-				switch (text[i])
-				{
-					case '(':
-					case '[':
-						stack.Push(text[i]);
-						break;
-					case ')':
-					//if (stack.Count == 0)
-					//{
-					//    throw new InvalidOperationException();
-					//}
-					//if (stack.Peek() == '(')
-					//{
-					//    stack.Pop();
-					//}
-					//break;
-					case ']':
-						if (stack.Count == 0)
-						{
-							return false;
-						}
-						if (stack.Peek() == '[' || stack.Peek() == '[')
-						{
-							stack.Pop();
-						}
-						else
-						{
-
-							return false;
-						}
-
-						break;
-					default:
-						break;
-				}
-				return true;
-
-			
-
-
-		}
-	}
+    class Program
+    {
+        static void Main()
+        {
+            var text = ReadText();
+            bool result = Check(text);
+            Console.WriteLine($"{ result}");
+            Console.ReadKey();
+        }
+        static string ReadText()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите строку:");
+                var text = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
+                    continue;
+                }
+                return text;
+            }
+        }
+        static bool Check(string text)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < text.Length;)
+            {
+                switch (text[i])
+                {
+                    case '(':
+                    case '[':
+                        stack.Push(text[i]);
+                        break;
+                    case ')':
+                    case ']':
+                        if (stack.Count == 0)
+                        {
+                            return false;
+                        }
+                        if (stack.Peek() == '[' || stack.Peek() == '[')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+            
+        }
+    }
 }
