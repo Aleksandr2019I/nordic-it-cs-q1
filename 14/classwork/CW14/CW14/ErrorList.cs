cf0bd64556ebf401f1b73757392a85a55a1bb299
MakarevichAV <redishtale@gmail.com>
2019-10-31 21:59:22 +0300

    rr

---
+++ 14/classwork/CW14/CW14/ErrorList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace CW14
+{
+	public class ErrorList : IDisposable,IEnumerable<string>
+	{
+
+
+		public string Category
+		{
+			get;
+		}
+
+		private List<string> _errors;
+
+
+
+		public ErrorList(string category, List<string> errors)
+		{
+			Category = category;
+			_errors = errors;
+		}
+
+		public void Dispose()
+		{
+			_errors.Clear();
+			_errors = null;
+		}
+
+		public void Add(string errorMessage)
+		{
+			_errors.Add(errorMessage);
+
+		}
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			return _errors.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
---
+++ 14/classwork/CW14/CW14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CW14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (ErrorList errorList = new ErrorList("ошибка",new List<string>()))
+			{
+				errorList.Add("onex");
+				errorList.Add("Twox");
+
+
+				foreach (var item in errorList)
+				{
+					Console.WriteLine($"{item}");
+				}
+			}
+
+
+			Console.ReadKey();
+		}
+	}
+}
