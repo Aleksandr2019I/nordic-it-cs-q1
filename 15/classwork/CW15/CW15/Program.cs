b68bfc1234e35d108c6087d4bf3ccb6e0e92476a
MakarevichAV <redishtale@gmail.com>
2019-11-05 21:56:06 +0300

    classwork

--- 15/classwork/CW15/CW15/Program.cs
+++ 15/classwork/CW15/CW15/Program.cs
@@ -5,36 +5,71 @@ namespace CW15
 {
 	class Program
 	{
+		static public double Area(double _radius)
+		{
+			return Math.PI*Math.Pow(_radius,2);
+
+			
+		}
+		static public double perimtr(double _radius)
+		{
+			return 2*Math.PI*_radius;
+
+
+		}
 		static void Main(string[] args)
 		{
-			var first = new Account<int>(12,"po");
-			var second = new Account<string>("text","text2");
-			var guides = new Account<Guid>(Guid.NewGuid(), "pff");
-			first.WriteProperties();
-			second.WriteProperties();
-			guides.WriteProperties();
+			//var first = new Account<int>(12,"po");
+			//var second = new Account<string>("text","text2");
+			//var guides = new Account<Guid>(Guid.NewGuid(), "pff");
+			//first.WriteProperties();
+			//second.WriteProperties();
+			//guides.WriteProperties();
+
+
+			var first = new Circle(1.5);
+			first.Calculate(Area);
+			first.Calculate(perimtr);
+
+			Console.ReadLine($"{first.}");
+
 			Console.ReadKey();
 		}
 	}
+	//class Account<T>
+	//{
+	//	public  T Id { get; private set; }
+	//	public string Name { get; private set; }
+	//	public Account( T id, string name)
+	//	{
+	//		Id = id;
+	//		Name = name;
+	//	}
+	//	public void WriteProperties()
+	//	{
+	//		Console.WriteLine($"id - {Id},name - {Name}");
 
-	class Account<T>
+	//	}
+	//}
+
+	public sealed class Circle
 	{
-		public  T Id { get; private set; }
-		public string Name { get; private set; }
+		private double _radius;
 
-		public Account( T id, string name)
+		public Circle(double radius)
 		{
-			Id = id;
-			Name = name;
-		}
-		public void WriteProperties()
-		{
-			Console.WriteLine($"id - {Id},name - {Name}");
+			_radius = radius;
 
 		}
 
+		public double Calculate(Func<double, double> operation)
+		{
+			return operation(_radius);
+
+		}
 
 	
+
 	}
 	
 }
