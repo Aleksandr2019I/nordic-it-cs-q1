425dbab84996eb000348b7dcd2d1479364546b1a
aleksandr2019i <igr0k@ya.ru>
2019-11-13 21:32:40 +0300

    readfilename

--- 15/HomeWork/HW15/HW15/Program.cs
+++ 15/HomeWork/HW15/HW15/Program.cs
@@ -11,20 +11,29 @@ namespace HW15
     class Program
     {
         static void Main()
-
         {
-            var filename = Console.ReadLine();
-			var first = new ConsoleLogWriter();
-			first.LogError("первое сообщение в консоль");
-			var second = new FileLogWriter(filename);
-			second.LogInfo(" запись в файл");
-			var third = new MultipleLogWriter(first, second);
-			third.LogWarning("и туда и туда");
-
-
-			
-
+            var filename = ReadFileName();
+            var first = new ConsoleLogWriter();
+            first.LogError("первое сообщение в консоль");
+            var second = new FileLogWriter(filename);
+            second.LogInfo(" запись в файл");
+            var third = new MultipleLogWriter(first, second);
+            third.LogWarning("и туда и туда");
             Console.ReadKey();
+        }
+        static string ReadFileName()
+        {
+            while (true)
+            {
+                Console.WriteLine("введите путь файла");
+                var filename = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(filename))
+                {
+                    Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
+                    continue;
+                }
+                return filename;
+            }
         }
     }
 }
