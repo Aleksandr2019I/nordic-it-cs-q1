e3bc7f4ac7a6b6a327f94e647541461fd16d7329
aleksandr2019i <igr0k@ya.ru>
2019-11-18 00:03:02 +0300

    как сделать через свитч

--- 15/HomeWork/HW15/HW15/Logs/LogWriterFactory.cs
+++ 15/HomeWork/HW15/HW15/Logs/LogWriterFactory.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 namespace HW15
 {
-    class LogWriterFactory
+    public class LogWriterFactory
     {
         public ILogWriter GetLogWriter<T>(object parameters) where T : ILogWriter
         {
-            if (typeof(T) == typeof(ConsoleLogWriter))
-            {
+            switch ()
+            { 
+                case typeof(ConsoleLogWriter):
                 return new ConsoleLogWriter();
+                default:
+                    throw new InvalidOperationException("Ошибка операции");
             }
-            if (typeof(T) == typeof(FileLogWriter))
-            {
-                return new FileLogWriter((string)parameters);
-            }
-            if (typeof(T) == typeof(MultipleLogWriter))
-            {
-                return new MultipleLogWriter(new ConsoleLogWriter(), new FileLogWriter((string)parameters));
-            }
-            throw new InvalidOperationException("Ошибка операции");
+            //if (typeof(T) == typeof(ConsoleLogWriter))
+            //{
+            //    return new ConsoleLogWriter();
+            //}
+            //if (typeof(T) == typeof(FileLogWriter))
+            //{
+            //    return new FileLogWriter((string)parameters);
+            //}
+            //if (typeof(T) == typeof(MultipleLogWriter))
+            //{
+            //    return new MultipleLogWriter(new ConsoleLogWriter(), new FileLogWriter((string)parameters));
+            //}
+            //throw new InvalidOperationException("Ошибка операции");
         }
         private static LogWriterFactory _instance;
         public static LogWriterFactory Instance
