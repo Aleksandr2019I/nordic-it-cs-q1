c1d6c174beddcc201b5ea7ecbd30dfc81cfd90b5
yar <yaroslavkuznetsov788@gmail.com>
2019-12-05 20:42:12 +0300

    123

---
+++ 24/classwork/ClassWork-12-03-19-20-49-34/CityApp/CityApp/Controllers/CityController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using CityApp.Models;
+using CityApp.Services;
+using CityApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CityApp.Controllers
+{
+    /// <summary>
+    ///  Контроллер, определяет логику по управлению данными городов
+    /// </summary>
+    public class CityController : Controller
+	{
+		private ILogger _logger;
+
+		private CityStorage _storage;
+
+
+
+		public CityController(ILogger logger, CityStorage storage)
+		{
+			_logger = logger;
+			_storage = storage;
+		}
+
+		// GET /cities
+		// GET /api/city/list
+		[HttpGet("cities")]
+		[HttpGet("api/city/list")]
+		public IActionResult List()
+		{
+			
+            var cities = _storage
+				.GetAll() // City[] -> IEnumerable<City>
+                .Select((City city) => new CityViewModel(city))
+                .OrderBy((CityViewModel viewModel) => viewModel.Name)
+                .ToArray();
+
+            return Ok(cities);
+		}
+
+        // GET /cities/id
+        // GET /api/city/id
+        [HttpGet("cities/{id}", Name = "Get")]
+        [HttpGet("api/city/{id}", Name = "ApiGet")]
+        public IActionResult Get(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+				_logger.LogWarning("Invalid id specified");
+                return BadRequest();
+            }
+
+            var city = _storage.GetById(id);
+            if (city == null)
+            {
+				_logger.LogWarning("City with id {0} is not found", id);
+				return NotFound();
+            }
+
+            return Ok(new DetailCityViewModel(city));
+        }
+
+		// POST /cities
+		// POST /api/city
+		[HttpPost("cities")]
+		[HttpPost("api/city")]
+		public IActionResult Create([FromBody] CreateCityViewModel city)
+		{
+            if (city == null)
+            {
+                return BadRequest();
+            }
+
+            var model = new City(
+                city.Name,
+                city.Description,
+                city.Polulation);
+
+			_storage.Create(model);
+
+            return CreatedAtAction("Get", model);
+		}
+	}
+}
---
+++ 24/classwork/ClassWork-12-03-19-20-49-34/CityApp/CityApp/Services/CityStorage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using CityApp.Models;
+
+namespace CityApp.Services
+{
+    /// <summary>
+    /// Хранилище городов
+    /// </summary>
+    public class CityStorage
+    {
+        private static CityStorage _instance;
+
+        public static CityStorage Instance =>
+            _instance ?? (_instance = new CityStorage());
+
+        private readonly List<City> _cities;
+
+        private CityStorage()
+        {
+            _cities = new List<City>
+            {
+                new City("Москва", "Столица России", 16_000_000),
+                new City("Санкт Петербург", "Северная столица России", 5_000_000)
+            };
+        }
+
+        /// <summary>
+        /// Метод возвращает список всех городов
+        /// </summary>
+        public City[] GetAll()
+        {
+            return _cities.ToArray();
+        }
+
+        public City GetById(Guid id)
+        {
+            return _cities.FirstOrDefault(city => city.Id == id);
+        }
+
+        /// <summary>
+        /// Метод добавляет город в коллекцию 
+        /// </summary>
+        public void Create(City city)
+        {
+            _cities.Add(city);
+        }
+    }
+}
