3659f8047acc0dfbb214ee8df5a75fb547eebe7f
potap <test@gmaol.com>
2019-12-10 20:28:11 +0300

    work

---
+++ 25/classwork/ClassWork-12-10-19-00-37-36/HomeWork/Reminder.Storage.WebApi/ReminderStorage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace Reminder.Storage.WebApi
+{
+	public class ReminderStorage : IReminderStorage
+	{
+
+		private readonly HttpClient _client;
+		public ReminderStorage(HttpClient client)
+		{
+			_client = client;
+
+		}
+
+		public void Add(ReminderItem item)
+		{
+			var json = JsonSerializer.Serialize(item);
+			var request = new HttpRequestMessage(HttpMethod.Post,"/api/reminders/")
+			{ 
+				Content = new StringContent(json, Encoding.UTF8,"application/json")
+			};
+
+			var response = _client.SendAsync(request)
+				.GetAwaiter()
+				.GetResult();
+
+			if (response.StatusCode == HttpStatusCode.Created)
+			{
+				throw new InvalidOperationException(
+					$"Operation failed with code :{response.StatusCode}");
+			}
+		}
+
+		public List<ReminderItem> FindBy(ReminderItemFilter filter)
+		{
+			
+		}
+
+		public ReminderItem FindById(Guid id)
+		{
+			
+		}
+
+		public void Update(ReminderItem item)
+		{
+			
+		}
+	}
+}
