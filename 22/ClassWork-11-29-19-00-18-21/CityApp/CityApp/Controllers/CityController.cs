98b33638d2b0224d98c6a2e4b28c1233b00390f1
aleksandr2019i <igr0k@ya.ru>
2019-12-16 19:17:17 +0300

    т.е guid id

--- 22/ClassWork-11-29-19-00-18-21/CityApp/CityApp/Controllers/CityController.cs
+++ 22/ClassWork-11-29-19-00-18-21/CityApp/CityApp/Controllers/CityController.cs
@@ -4,138 +4,122 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CityApp.Controllers
 {
-	public class City
-	{
-		public Guid Id { get; set; }
-		public string Name { get; set; }
-		public int Population { get; set; }
-	}
+    public class City
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+    }
 
-	/// <summary>
-	/// Хранилище городов
-	/// </summary>
-	public class CityStorage
-	{
+    /// <summary>
+    /// Хранилище городов
+    /// </summary>
+    public class CityStorage
+    {
+        private static CityStorage _instance;
+        public static CityStorage Instance =>
+            _instance ?? (_instance = new CityStorage());
 
-		private static CityStorage _instance;
-
-		public static CityStorage Instance =>
-			_instance ?? (_instance = new CityStorage());
-
-		private readonly List<City> _cities;
-
-		private CityStorage()
-		{
-			_cities = new List<City>
-			{
-				new City
-				{
-					Id = Guid.NewGuid(),
-					Name = "Москва",
-					Population = 16_000_000
-				},
-				new City
-				{
-					Id = Guid.NewGuid(),
-					Name = "Санкт Петербург",
-					Population = 5_000_000
-				}
-			};
-		}
-
-		/// <summary>
-		/// Метод возвращает список всех городов
-		/// </summary>
-		public City[] GetAll()
-		{
-			return _cities.ToArray();
-		}
-
-		/// <summary>
-		/// Метод добавляет город в коллекцию 
-		/// </summary>
-		public void Create(City city)
-		{
-			_cities.Add(city);
-		}
-
-		/// <summary>
+        private readonly List<City> _cities;
+        private CityStorage()
+        {
+            _cities = new List<City>
+            {
+                new City
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Москва",
+                    Population = 16_000_000
+                },
+                new City
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Санкт Петербург",
+                    Population = 5_000_000
+                }
+            };
+        }
+        /// <summary>
+        /// Метод возвращает список всех городов
+        /// </summary>
+        public City[] GetAll()
+        {
+            return _cities.ToArray();
+        }
+        /// <summary>
+        /// Метод добавляет город в коллекцию 
+        /// </summary>
+        public void Create(City city)
+        {
+            _cities.Add(city);
+        }
+        /// <summary>
         /// Дома
-		/// Метод замены в коллекции
-		/// </summary>
-		public void Put(City city)
-		{
-
-			_cities.Remove(city);
-
-			_cities.Add(city);
-			
-		}
+        /// Метод замены в коллекции
+        /// </summary>
+        public void Put(City city)
+        {
+            _cities.Remove(city);
+            _cities.Add(city);
+        }
         /// <summary>
         /// дома
         /// Метод удаления в коллекции
         /// </summary>
         public void Delete(City city)
-		{
-			_cities.Remove(city);
-		}
+        {
+            _cities.Remove(city);
+        }
+    }
+    /// <summary>
+    ///  Контроллер, определяет логику по управлению данными городов
+    /// </summary>
+    public class CityController : Controller
+    {
+        // GET /cities
+        // GET /api/city/list
+        [HttpGet("cities")]
+        [HttpGet("api/city/list")]
+        public IActionResult List()
+        {
+            var cities = CityStorage.Instance.GetAll();
+            return Json(cities);
+        }
 
-	}
-
-	/// <summary>
-	///  Контроллер, определяет логику по управлению данными городов
-	/// </summary>
-	public class CityController : Controller
-	{
-		// GET /cities
-		// GET /api/city/list
-		[HttpGet("cities")]
-		[HttpGet("api/city/list")]
-		public IActionResult List()
-		{
-			var cities = CityStorage.Instance.GetAll();
-			return Json(cities);
-		}
-
-		// POST /cities
-		// POST /api/city
-		[HttpPost("cities")]
-		[HttpPost("api/city")]
-		public IActionResult Create([FromBody] City city)
-		{
-			CityStorage.Instance.Create(city);
-			return Ok();
-		}
+        // POST /cities
+        // POST /api/city
+        [HttpPost("cities")]
+        [HttpPost("api/city")]
+        public IActionResult Create([FromBody] City city)
+        {
+            CityStorage.Instance.Create(city);
+            return Ok();
+        }
 
         //дома
-		[HttpPut("cities")]
-		[HttpPut("api/city/list")]
-		public IActionResult Put([FromBody] City city)
-		{
+        [HttpPut("cities")]
+        [HttpPut("api/city/Put")]
+        public IActionResult Put([FromBody] City city)
+        {
             if (city == null)
             {
-          
                 return BadRequest();
             }
-            
-            CityStorage.Instance.Put(city);
-			return Ok();
 
-		}
+            CityStorage.Instance.Put(city);
+            return Ok();
+        }
         //дома
-		[HttpDelete("cities")]
-		[HttpDelete("api/city/list")]
-		public IActionResult Delete([FromBody]City city )
-		{
+        [HttpDelete("cities")]
+        [HttpDelete("api/city/Delete")]
+        public IActionResult Delete([FromBody]City city)
+        {
             if (city == null)
             {
-
                 return BadRequest();
             }
             CityStorage.Instance.Delete(city);
-			return Ok();
-
-		}
-
-	}
+            return Ok();
+        }
+    }
 }
